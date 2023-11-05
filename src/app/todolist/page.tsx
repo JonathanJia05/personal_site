686924f9aab2faa73f4c3f3f24a9225a9e163085
Jonathan Jia <jonathanj18456@gmail.com>
2023-11-04 23:05:06 -0400

    made todo list page

---
+++ src/app/todolist/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function Todo() {
+  return (
+    <>
+      <header>
+        <h1 className="text-2xl">Todos</h1>
+      </header>
+      <ul></ul>
+    </>
+  );
+}
