d77a6882b23fb7ae5b6352e67b18bfd37610b892
Jonathan Jia <jonathanj18456@gmail.com>
2023-11-05 00:35:03 -0400

    created todo list page and setup database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
           Todo List
         </Link>
       </header>
-      <ul></ul>
     </>
   );
 }
--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -1,12 +1,40 @@
-import Link from "next/link"
+import Link from "next/link";
+import prisma from "../db";
+import { TodoItem } from "@/components/TodoItem";
 
-export default function Todo() {
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo(id: string, complete:boolean) {
+  "use server"
+  await prisma.todo.update({where: {id}, data:{complete}})
+}
+
+export default async function Todo() {
+  const todos = await getTodos()
   return (
     <>
-      <header>
+      <header className="flex justify-between">
         <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/newitem"
+        >
+          Add Item
+        </Link>
       </header>
-      <ul></ul>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
+        ))}
+      </ul>
     </>
   );
 }
